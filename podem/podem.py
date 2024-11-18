48b7412f4a97858f4a12a8f2d374c66036ddd305
jack-mil <62065280+jack-mil@users.noreply.github.com>
2024-11-18 00:58:11 -0500

    feat: implement TestGenerator class using PODEM algo
    Technically works, needs more tests, and clean up + refactor

--- podem/podem.py
+++ podem/podem.py
@@ -4,19 +4,99 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from pathlib import Path
-    from typing import Literal
 
-    from simulator.structs import Fault, NetId
+    from simulator.structs import Fault, Gate, NetId
 
-from simulator.structs import Logic
-from simulator.circuit import Circuit
-from simulator.simulator import Simulation
+from simulator.simulator import BaseSim
+from simulator.structs import Gate, Logic
+
+
+class ErrorSim(BaseSim):
+    """TODO: docs"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fault: Fault = None
+
+    def _process_ready_gate(self, gate: Gate) -> Logic:
+        """TODO: docs"""
+        output_state = super()._process_ready_gate(gate)
+
+        # skip if no target set
+        if self.fault is None:
+            return output_state
+        # inject the error state on the target net
+        if gate.output == self.fault.net_id:
+            if output_state is Logic.High and self.fault.stuck_at is Logic.Low:
+                output_state = Logic.D
+            elif output_state is Logic.Low and self.fault.stuck_at is Logic.High:
+                output_state = Logic.Dbar
+        return output_state
+
+    def start_state(self, fault: Fault):
+        """Set internal state ready to do simulations for PODEM"""
+        self.fault = fault
+        self.reset()
+        # explicitly give X values to the all primary inputs
+        # in order to do 5-valued forward simulation to completion
+        for net_id in self.circuit.inputs:
+            self._net_states[net_id] = Logic.X
+
+    def get_input_states(self) -> list[Logic]:
+        """List of circuit input values in the order of original net-list"""
+        inputs = [self.get_state(net) for net in self.circuit.inputs]
+
+        return inputs
+
+    def format_inputs(self) -> str:
+        """A string representation of the circuit input assignments"""
+        inputs = self.get_input_states()
+        inputs = [Logic.High if v is Logic.D else v for v in inputs]
+        inputs = [Logic.Low if v is Logic.Dbar else v for v in inputs]
+        input_str = ''.join(str(v) for v in inputs)
+        
+        return input_str
+
+    def simulate_input_assignment(self, pi_net: NetId, value: Logic):
+        """Assign a single primary input the given logic value."""
+        if pi_net not in self.circuit.inputs:
+            raise ValueError(f'Net {pi_net} is not a Primary Input (PI)')
+
+        # save the previous input assignments before resetting internal assignments
+        prev_inputs = self.get_input_states()
+        self.reset()
+        # assign old input values
+        for net_id, state in zip(self.circuit.inputs, prev_inputs, strict=True):
+            self._net_states[net_id] = state
+        # overwrite with the new input assignment
+        if self.fault and pi_net == self.fault.net_id:
+            self._net_states[pi_net] = Logic.D if self.fault.stuck_at is Logic.Low else Logic.Dbar
+        else:
+            self._net_states[pi_net] = value
+        # perform a full forward simulation to update state
+        self._make_implications()
+
+    def build_d_frontier(self) -> set[Gate]:
+        """
+        Return all gates that currently have unset (X) output,
+        but one or more D/D̅ values on inputs.
+        """
+        d_frontier = set()
+        for gate in self.circuit.gates:
+            out_value = self.get_state(gate.output)
+            if out_value is Logic.X:
+                in_values = (self.get_state(net) for net in gate.inputs)
+                if any(val is Logic.D or val is Logic.Dbar for val in in_values):
+                    d_frontier.add(gate)
+        return d_frontier
 
 
 class TestGenerator:
     """
-    Use the PODEM (Path-Oriented Decision Making) Automatic Test Pattern Generation algorithm
-    to generate test vectors for specific stuck-at faults on a given net-list of combinational logic gates.
+    Use the PODEM (Path-Oriented Decision Making)
+    Automatic Test Pattern Generation algorithm
+    to generate test vectors for specific stuck-at faults
+    on a given net-list of combinational logic gates.
 
     The class uses the same Circuit and Simulation primitives as the fault simulator.
     """
@@ -26,20 +106,85 @@ class TestGenerator:
         Initialize a new test generator for the circuit in file `netlist`.
 
         Tests can be generated for multiple faults using the same TestGenerator object.
-
         Optionally, load from a list of strings in the format of net-list file lines (for testing)
         """
 
-        self.sim = Simulation(netlist)
-        """Static, state-less representation of the topology of the circuit (gates and net ids)"""
+        self.sim = ErrorSim(netlist)
+        """TODO: add docs"""
 
+        self.d_frontier: set[Gate] = set()
+        """Gates whose output is unset (X) and at-least one input is D or D̅"""
+
+        # TODO: rename
         self.gate_output_nets = {gate.output: gate for gate in self.sim.circuit.gates}
         """Mapping of a particular net to the Gate driving it."""
 
-    def generate_tests(self, fault: Fault):
-        return set()
+    def generate_test(self, fault: Fault):
+        # reset and prepare forward-simulation engine
+        self.sim.start_state(fault)
 
-    def backtrace(self, net: NetId, state: Literal[Logic.Low, Logic.High]):
+        # recursively execute the PODEM algorithm
+        if self.podem(fault):
+            # succeeded, return test as a string
+            test_vector = self.sim.format_inputs()
+            return test_vector
+
+        # undetectable fault
+        return None
+
+    def podem(self, fault: Fault) -> bool:
+        """Very literal Textbook PODEM algorithm with no optimization."""
+        if self.check_success():
+            return True
+
+        if self.check_failure(fault):
+            return False
+
+        net, value = self.objective(fault)
+        pi_net, value = self.backtrace(net, value)  # produces a Primary Input assignment
+
+        self.imply(pi_net, value)  # forward simulate
+        if self.podem(fault):  # recurse and try again
+            return True
+
+        self.imply(pi_net, ~value)  # reverse decision
+        if self.podem(fault):  # recurse and try again
+            return True
+
+        # reset implication, path exhausted
+        self.imply(pi_net, Logic.X)
+        return False
+
+    def imply(self, pi_net: NetId, value: Logic):
+        # first simulate
+        self.sim.simulate_input_assignment(pi_net, value)
+
+        # maintain D frontier after each input simulation
+        self.d_frontier = self.sim.build_d_frontier()
+
+    def objective(self, target_fault: Fault):
+        """
+        Produce a net id and objective state for the target fault.
+
+        First the target fault is activated; then resulting error is
+        propagated towards a primary output (pg. 204).
+        """
+
+        # activate the target fault with opposite value
+        if self.sim.get_state(target_fault.net_id) is Logic.X:
+            return target_fault.net_id, ~target_fault.stuck_at
+
+        # pick a gate from d-frontier
+        gate = next(iter(self.d_frontier))
+        # pick an unassigned input of the gate (x-path)
+        net = self.pick_unset_input(gate)
+        control_value = gate.control_value()
+        assert control_value is not None  # Inv/Buf should never be in the d-frontier
+        # objective is opposite of the controlling value
+        # in order to propagate error on other gate input(s)
+        return net, ~control_value
+
+    def backtrace(self, net: NetId, state: Logic):
         """
         Find a primary input (PI) assignment that is likely to contribute to
         setting the objective net (`net`) to a Logic High or Low (`state`).
@@ -53,12 +198,40 @@ class TestGenerator:
             driving_gate = self.gate_output_nets[net]
             # keep track of the inversion parity of the path
             state = state ^ driving_gate.inversion()
-            # choose the first gate input with unassigned value (x-path)
-            net = next(
-                input for input in driving_gate.inputs if self.sim.get_net_state(input) is Logic.X
-            )
-            # loop
+            # pick an unassigned input of the gate (x-path)
+            net = self.pick_unset_input(driving_gate)
+        # loop
 
         # net_id is a primary input
         assert net in self.sim.circuit.inputs
         return net, state
+
+    def check_success(self) -> bool:
+        """Return True if the fault has been detected"""
+        # succeed if a primary output has a D or D̅
+        if any(value is Logic.D or value is Logic.Dbar for value in self.sim.get_output_states()):
+            return True
+
+        # cannot determine
+        return False
+
+    def check_failure(self, fault: Fault) -> bool:
+        """Return True if detecting the fault is impossible"""
+        # faulty net cannot be activate
+        if self.sim.get_state(fault.net_id) is fault.stuck_at:
+            return True
+
+        # D-frontier is empty, fault cannot be propagated
+        if self.sim.get_state(fault.net_id) is not Logic.X and len(self.d_frontier) == 0:
+            return True
+
+        # Error propagation look-ahead (x-path check)
+        # TODO: maybe in future implementation
+
+        # might be detectible
+        return False
+
+    def pick_unset_input(self, gate: Gate) -> NetId:
+        """Get an arbitrary net with unassigned value from `gate`"""
+        net = next(input for input in gate.inputs if self.sim.get_state(input) is Logic.X)
+        return net
--- tests/test_podem.py
+++ tests/test_podem.py
@@ -1,28 +1,132 @@
 import unittest
 
-from simulator import Fault, Logic
-
 from podem import TestGenerator
+from simulator.structs import Fault, Gate, GateType, Logic
+
 
 class TestPodemUnits(unittest.TestCase):
     def test_backtrace(self):
-        netlist = [ # example circuit from textbook Figure 6.28
+        netlist = [  # example circuit from textbook Figure 6.28
             'INV a c',
             'NAND c b d',
             'INV d f',
             'INPUT a b -1',
-            'OUTPUT f -1'
+            'OUTPUT f -1',
+        ]
+        objective = ('f', Logic.High)  # try to set net f to logic '1'
+
+        podem = TestGenerator(netlist)
+        pi_net, value = podem.backtrace(*objective)
+        print(f'{pi_net=}, {value=}')
+
+        self.assertEqual(pi_net, 'a')
+        self.assertEqual(value, Logic.Low)
+
+        # manually make implications
+        podem.sim._net_states['a'] = Logic.Low
+        podem.sim._net_states['c'] = Logic.High
+
+        # assigning just a=0 does not achieve objective
+        # try again
+        pi_net, value = podem.backtrace(*objective)
+        print(f'{pi_net=}, {value=}')
+        # should come up with b=1
+        self.assertEqual(pi_net, 'b')
+        self.assertEqual(value, Logic.High)
+
+    def test_objective_simple(self):
+        netlist = [  # example circuit from textbook Figure 6.28
+            'INV a c',
+            'NAND c b d',
+            'INV d f',
+            'INPUT a b -1',
+            'OUTPUT f -1',
+        ]
+        target_fault = Fault('f', Logic.Low)
+
+        podem = TestGenerator(netlist)
+        net, value = podem.objective(target_fault)
+
+        # first time should pick opposite of fault
+        self.assertEqual(net, 'f')
+        self.assertEqual(value, Logic.High)
+
+        net, value = podem.objective(target_fault)
+
+    def test_objective_two_steps(self):
+        netlist = [  # example circuit from textbook Figure 6.28
+            'INV a d',
+            'AND b d e',
+            'NOR e c f',
+            'INPUT a b c -1',
+            'OUTPUT f -1',
+        ]
+        target_fault = Fault('b', Logic.Low)
+
+        podem = TestGenerator(netlist)
+
+        # manually set state for next objective
+        podem.sim._net_states['e'] = Logic.X
+        podem.sim._net_states['d'] = Logic.X
+        podem.sim._net_states['b'] = Logic.D
+        podem.d_frontier = {Gate(GateType.And, ('b', 'd'), 'e')}
+
+        net, value = podem.objective(target_fault)
+        # next objective should try to propagate error to net 'e'
+        self.assertEqual(net, 'd')
+        self.assertEqual(value, Logic.High)
+
+        # continues until D reaches primary output
+
+    def test_imply(self):
+        netlist = [  # example circuit from textbook Figure 6.28
+            'INV a c',
+            'NAND c b d',
+            'INV d f',
+            'INPUT a b -1',
+            'OUTPUT f -1',
         ]
         podem = TestGenerator(netlist)
 
-        objective = ('f', Logic.High) # output net 6 at logic '1'
+        pi_assignement = ('a', Logic.Low)
+        podem.imply(*pi_assignement)
 
-        primary_in_net, value = podem.backtrace(*objective)
-        print(f'{primary_in_net=}, {value=}')
+        self.assertEqual(podem.sim.get_state('a'), Logic.Low)
+        self.assertEqual(podem.sim.get_state('c'), Logic.High)
+        self.assertEqual(podem.sim.get_state('b'), Logic.X)
+        self.assertEqual(podem.sim.get_state('d'), Logic.X)
 
-        self.assertEqual(primary_in_net, 'a')
-        self.assertEqual(value, Logic.Low)
+        # next input assignment builds on last
+        pi_assignement = ('b', Logic.High)
+        podem.imply(*pi_assignement)
 
-        # need to imply() a=0 and run backtrace() again
-        # should come up with b=1
+        self.assertEqual(podem.sim.get_state('a'), Logic.Low)
+        self.assertEqual(podem.sim.get_state('c'), Logic.High)
+        self.assertEqual(podem.sim.get_state('b'), Logic.High)
+        self.assertEqual(podem.sim.get_state('d'), Logic.Low)
+        self.assertEqual(podem.sim.get_state('f'), Logic.High)
 
+
+class TestSimplePodem(unittest.TestCase):
+    def test_and_gate(self):
+        """Generate a test vector for a single AND gate"""
+        netlist = [
+            'AND 1 2 3',
+            'INPUT 1 2 -1',
+            'OUTPUT 3 -1',
+        ]
+        tests_for_faults = {
+            Fault(3, Logic.High): {'0X', 'X0'},
+            Fault(3, Logic.Low): {'11'},
+            Fault(1, Logic.Low): {'11'},
+            Fault(2, Logic.Low): {'11'},
+            Fault(1, Logic.High): {'01'},
+            Fault(2, Logic.High): {'10'},
+        }
+
+        podem = TestGenerator(netlist)
+
+        for fault, expected_tests in tests_for_faults.items():
+            with self.subTest(test=str(fault)):
+                found_test = podem.generate_test(fault)
+            self.assertIn(found_test, expected_tests)
