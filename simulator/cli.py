d536064534a70dacc25e66437b535a947fcc4276
jack-mil <62065280+jack-mil@users.noreply.github.com>
2024-11-25 00:06:41 -0500

    feat: add 'generate' to cli
    Signed-off-by: jack-mil <62065280+jack-mil@users.noreply.github.com>

--- simulator/cli.py
+++ simulator/cli.py
@@ -1,5 +1,6 @@
 """
-Circuit Simulator and Deductive Fault Simulator entry point and command line interface.
+Command line interface and interactive entry-point
+for the Fault Simulator and PODEM Test Generator.
 This module shows how to use the `simulator` and `circuit` module API.
 """
 
@@ -11,7 +12,9 @@ if TYPE_CHECKING:
     from argparse import Namespace
     from pathlib import Path
 
+from . import util
 from .faultsim import FaultSimulation
+from .podem import TestGenerator
 from .simulator import Simulation
 
 
@@ -20,27 +23,30 @@ def main():
 
     # support a single input vector from cli arguments,
     # or read multiple vectors from a file
-    if args.input_file:
-        with args.input_file.open() as fp:
-            tests = [line.rstrip() for line in fp if line]
+    if args.command == 'generate':
+        generate(args.net_file, args.faults)
     else:
-        tests = [args.input_vector]
+        if args.input_file:
+            with args.input_file.open() as fp:
+                tests = [line.rstrip() for line in fp if line]
+        else:
+            tests = [args.input_vector]
 
-    if args.command == 'simulate':
-        simulate(args.net_file, tests)
-    if args.command == 'faults':
-        deduce(args.net_file, tests)
+        if args.command == 'simulate':
+            simulate(args.net_file, tests)
+        elif args.command == 'faults':
+            deduce(args.net_file, tests)
 
 
 def simulate(net_file: Path, tests: list[str]):
     """Action for running fault-free circuit simulator."""
-    # Create the simulation object from this circuit
+    # Create the simulation object for this circuit
     circuit = Simulation(net_file)
 
     # Run multiple test vectors with the same object
     # and print the output nicely
     print(f'Circuit: {net_file}')
-    print(f"{'Inputs'.ljust(len(tests[0]))} | {'Outputs'}")
+    print(f'{"Inputs".ljust(len(tests[0]))} | {"Outputs"}')
     for test in tests:
         out = circuit.simulate_input(test)
         print(f'{test} | {out}')
@@ -49,7 +55,7 @@ def simulate(net_file: Path, tests: list[str]):
 
 def deduce(net_file: Path, tests: list[str]):
     """Action for running deductive fault simulator."""
-    # Create the deductive fault simulator from this net-list
+    # Create the deductive fault simulator for this circuit
     sim = FaultSimulation(net_file)
 
     # Run multiple test vectors with the same object
@@ -62,6 +68,36 @@ def deduce(net_file: Path, tests: list[str]):
         for f in sorted(faults):
             print(f'{f.net_id:>4} stuck at {f.stuck_at}')
         print()
+    print(flush=True)
+
+
+def generate(net_file: Path, target_faults: list[str]):
+    """Generate a test vector for fault in format '[net-id]-sa-[0 | 1]'. e.g. 2-sa-0"""
+
+    import sys
+
+    # pre-process the list of faults to turn into list of Faults
+    faults = []
+    for string in target_faults:
+        fault = util.str_to_fault(string)
+        if fault is None:
+            print(f'Fault {string!r} is invalid.', file=sys.stderr)
+            print(
+                'Format: [net-id]-sa[0|1]. E.g. 2-sa-0, net123-sa-0, etc.',
+                file=sys.stderr,
+            )
+            exit(1)
+        faults.append(fault)
+    faults.sort()
+    # Create the PODEM ATP Generator for this net-circuit
+    gen = TestGenerator(net_file)
+
+    width = max(len(s) for s in target_faults)
+    print(f'Circuit: {net_file}')
+    print(f'{"Fault".ljust(width)} | Test')
+    for fault in faults:
+        test = gen.generate_test(fault)
+        print(f'{f"{fault}":<{width}} | {test if test else "UNDETECTABLE"}')
     print(flush=True)
 
 
@@ -110,6 +146,15 @@ def parse_args() -> Namespace:
         'faults', help='Find detected faults with a given net-list and test vector(s)'
     )
     add_common_arguments(faults_parser)
+
+    # Subcommand 'generate'
+    podem_parser = subparsers.add_parser(
+        'generate', help='Find a test vector that detects the given fault'
+    )
+    podem_parser.add_argument('net_file', type=valid_path, help='Net-list file (circuit) to test')
+    podem_parser.add_argument(
+        'faults', nargs='+', help='One or more faults to generate tests for (5-sa0)'
+    )
 
     return parser.parse_args()
 
