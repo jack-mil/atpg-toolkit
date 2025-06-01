e3bb86cfbe62225045f249370953b46dada0276a
jack-mil <62065280+jack-mil@users.noreply.github.com>
2025-06-01 18:40:11 -0400

    chore: remove old project leftovers from master branch
    Will remain archived on the old-project branch

--- scripts/demo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# used for demonstrating the CLI with on-damand fault generation
-set -x
-./atpg-toolkit generate circuits/s349f_2.net 185-sa-1
-./atpg-toolkit faults circuits/s349f_2.net 01XX11XXXX1XXXX0XXXXXXXX
-./atpg-toolkit generate circuits/s344f_2.net 95-sa-0
-./atpg-toolkit faults circuits/s344f_2.net X1XXXXXXXXX1XXXXXXXXXXXX
-./atpg-toolkit generate circuits/s349f_2.net 179-sa-1
-./atpg-toolkit generate circuits/s344ff_2.txt 191-sa-0
